f8cfaee6ae9e1e026dc7d3d0b29878e8dc44c1e4
Pablo Sao <pablo_sao@outlook.com>
2020-02-17 14:33:19 -0600

    Update MovieAnalytics.py

--- MovieAnalytics.py
+++ MovieAnalytics.py
@@ -10,6 +10,13 @@ import pandas as pd
 # Cargando data
 data = pd.read_csv('tmdb-movies.csv')
 
+#data = data.dropna()
+
+# data['release_date'] =pd.to_datetime(data['release_date'])
+
+#data.sort_values(by='release_date', inplace=True, ascending=False)
+
+
 data["runtime"]=data["runtime"].div(60).round(2)
 
 #calculando la ganancia
@@ -39,7 +46,7 @@ X = data["release_date"]
 Y = data["monto_ganancia"]
 
 serie_tiempo = px.line(data, x=X, y=Y,color="release_year",
-                       hover_data=['original_title'])
+                       hover_data=['original_title','monto_ganancia'])
 
 serie_tiempo.update_layout(title='Ganancia vs Fecha Estreno')
 serie_tiempo.update_yaxes(title_text="Ganancia (Ingresos - Presupuesto)")
